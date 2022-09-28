12845fa228cfc49159ee50eba2840bf862b751ba
Himanshu Dixit <101091797+himanshudixit1701@users.noreply.github.com>
2022-09-28 09:00:36 +0530

    Add files via upload

---
+++ basicInputOutput.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int num1, num2;
+    cout<<"enter the value of num1:\n"; /*<< is called insertion operator*/
+    cin>>num1; /*>> is called extraction operator*/
+
+    cout<<"Enter the value of num2:\n";
+    cin>>num2;
+
+    cout<<"the sum is "<< num1+num2;
+    
+     return 0;
+}
