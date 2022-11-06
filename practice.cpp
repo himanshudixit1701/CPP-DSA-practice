97b29fc33bd84e3b07d6f7a180b5f233b1cdc7d0
Himanshu Dixit <101091797+himanshudixit1701@users.noreply.github.com>
2022-11-06 13:19:06 +0530

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   int a, b;
+   cout<<"Enter the value of a"<<endl;
+   cin>>a;
+   cout<<"Enter the value of b"<<endl;
+   cin>>b;
+   if (a>b)
+   {
+        cout<<"a is greater then b"<<endl;
+   }else{
+    cout<<"a is less then b"<<endl;
+   }
+   
+   return 0;
+}
