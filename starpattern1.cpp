5a602c0bc47205c9e3edff82040267ffc3f0beab
Himanshu Dixit <101091797+himanshudixit1701@users.noreply.github.com>
2022-11-10 14:00:36 +0530

    Add files via upload

---
+++ starpattern1.cpp
@@ -0,0 +1,27 @@
+// star pattern
+// ****
+// ****
+// ****
+// ****
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   int n;
+   cout<<"Enter the value of n"<<endl;
+   cin>>n;
+   int i = 1;
+   while (i<=n)
+   {
+        int j = 1;
+        while(j<=n){
+            cout<<"*";
+            j++;
+        }
+        cout<<endl;
+        i++;
+   }
+   
+   return 0;
+}
