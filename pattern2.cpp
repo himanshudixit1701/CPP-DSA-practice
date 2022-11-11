72e63eb795a44d5cf89df2f3252626230b991f0a
Himanshu Dixit <101091797+himanshudixit1701@users.noreply.github.com>
2022-11-11 21:50:08 +0530

    Add files via upload

---
+++ pattern2.cpp
@@ -0,0 +1,25 @@
+// pattern
+// 1111
+// 2222
+// 3333
+// 4444
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   int n;
+   cout<<"Enter the value of n"<<endl;
+   cin>>n;
+   int i = 1;
+   while(i<=n){
+        int j = 1;
+        while(j<=n){
+            cout<<i;
+            j++;
+        }
+        cout<<endl;
+        i++;
+   }
+   return 0;
+}
